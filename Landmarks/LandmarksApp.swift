c7a053cf2fd2e064fb86c45b23c42ae32ffa10c9
gabizorzo <gabriela.zorzo@edu.pucrs.br>
2021-04-07 09:20:54 -0300

    landmarks

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Gabriela Zorzo on 24/03/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
